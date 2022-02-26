f66d35b4d924c7ddc111825dc0490cecb6cbb2d9
sheelaweswar <100165266+sheelaweswar@users.noreply.github.com>
2022-02-26 07:45:05 -0800

    Add files via upload

---
+++ EditLeadcls.java
@@ -0,0 +1,53 @@
+package Week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EditLeadcls {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver objcrmdriver = new ChromeDriver();
+		objcrmdriver.get("http://leaftaps.com/opentaps/control/login");
+		objcrmdriver.manage().window().maximize();
+		WebElement usrnme =  objcrmdriver.findElement(By.id("username"));
+		usrnme.sendKeys("demoSalesManager");
+	    objcrmdriver.findElement(By.id("password")).sendKeys("crmsfa");
+	    objcrmdriver.findElement(By.className("decorativeSubmit")).click();
+	    objcrmdriver.findElement(By.linkText("CRM/SFA")).click();
+	    objcrmdriver.findElement(By.linkText("Leads")).click();
+	    objcrmdriver.findElement(By.linkText("Create Lead")).click();
+	    WebElement cmpnme = objcrmdriver.findElement(By.id("createLeadForm_companyName"));
+	    cmpnme.sendKeys("TestLeaf");
+	    WebElement fstnme = objcrmdriver.findElement(By.id("createLeadForm_firstName"));
+	    fstnme.sendKeys("Sheelaw");
+	    WebElement lstnme = objcrmdriver.findElement(By.id("createLeadForm_lastName"));
+	    lstnme.sendKeys("Eswaramoorthy");
+	    WebElement dptnme = objcrmdriver.findElement(By.id("createLeadForm_departmentName"));
+	    dptnme.sendKeys("IT");
+	   	WebElement dcrptn = objcrmdriver.findElement(By.id("createLeadForm_description"));
+	    dcrptn.sendKeys("Creating the test lead");
+	    WebElement email = objcrmdriver.findElement(By.id("createLeadForm_primaryEmail"));
+	    email.sendKeys("sheelaw@gmail.com");
+	    WebElement stnme = objcrmdriver.findElement(By.name("generalStateProvinceGeoId"));
+	    Select objchoosest = new Select(stnme);
+	    objchoosest.selectByVisibleText("New York");
+	    objcrmdriver.findElement(By.className("smallSubmit")).click();
+	    objcrmdriver.findElement(By.linkText("Edit")).click();
+	    WebElement updcrptn = objcrmdriver.findElement(By.id("updateLeadForm_description"));
+	    updcrptn.clear();
+	    WebElement imptnote = objcrmdriver.findElement(By.name("importantNote"));
+	    imptnote.sendKeys("Type the important info here");
+	    String edleadtitle = objcrmdriver.getTitle();
+	    System.out.println(edleadtitle);
+	    objcrmdriver.findElement(By.className("smallSubmit")).click();
+	    String edleadtitle1 = objcrmdriver.getTitle();
+	    System.out.println(edleadtitle1);
+	    objcrmdriver.close();
+
+	}
+
+}
